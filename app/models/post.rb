23cf62bcfa077447f0d3b6b61d86f35af5435740
iameunji <touriste.polar@gmail.com>
2015-12-01 17:11:16 +0900

    add s3

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ActiveRecord::Base
 	serialize :date, Array
 
+	mount_uploader :img, ClassimgUploader
+
+
 	has_many :applies
 	has_many :users, :through => :applies
 	has_many :comments
